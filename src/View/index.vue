f02267183316a6bd3b446681e70ad3b1a9299540
unknown <bpxgy12138@163.com>
2020-12-22 23:55:12 +0800

    master 初始化

--- src/View/index.vue
+++ src/View/index.vue
@@ -2,14 +2,6 @@
 <template>
   <div>
       <h3>主页面</h3>
-      <div>
-<<<<<<< HEAD
-          <div class="sty">123</div>
-          <span>没什么就是创建一个gti分支 并合并</span>
-=======
-          <a href="">这是在index的分支上推送</a>
->>>>>>> index
-      </div>
   </div> 
 </template>
 
